ec821d8879fc92b988ff23cc2b60dbd02b0db82d
Yansir <markyan@foxmail.com>
2023-03-31 20:05:58 +0800

    sessionFuncitonModify

--- golem.py
+++ golem.py
@@ -17,8 +17,9 @@ class Golem:
         self.__init_sys_prompt = [{'role':'system', 'content':sys_prompt_prefix + sys_prompt + sys_prompt_suffix}]
 
     def response(self, user_input):
-        transcript_history = session.get('transcript_history', self.__init_sys_prompt)
-        if not self.__memory:
+        if self.__memory:
+            transcript_history = session.get('transcript_history', self.__init_sys_prompt)
+        else:
             transcript_history = self.__init_sys_prompt
         transcript_history += [{'role':'user', 'content':self.__user_input_prefix + user_input + self.__user_input_suffix}]
         openai.api_key = self.__openai_api_key
