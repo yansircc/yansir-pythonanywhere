d4dc552d1f817994a4194287b9e62fe282b13967
yansir <markyan@foxmail.com>
2023-04-06 18:05:52 +0800

    simpleWSGI

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,8 @@
 from flask_app import app
 from dotenv import load_dotenv
-from werkzeug.wrappers import Request, Response
-from werkzeug.serving import run_simple
 
 
 load_dotenv()
-application = app.wsgi_app
 
 if __name__ == '__main__':
-    run_simple('localhost', 5000, application)
+    app.run()
